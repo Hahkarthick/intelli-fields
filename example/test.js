7f6ecebf9f1d0a9be33b56278e3dbd36478f8ef8
Harish_vastpro <harish@vastpro.in>
2020-07-08 21:21:34 +0530

    Harish - Clean up the dependencies

--- example/test.js
+++ example/test.js
@@ -4,13 +4,17 @@ import IntelliFields from '..'
 
 export default class Test extends Component {
     static propTypes = {
-        
+
     }
 
     render() {
         return (
             <div>
-                <IntelliFields type="text" dataValidationRequired="true" dataErrorMsg="this is mandatory"/>
+                <IntelliFields type="text" dataValidationRequired="true" name="name" label="Name" dataErrorMsg="Name is Required" />
+                <IntelliFields type="text" label="Email Id"
+                 dataValidationRequired="true" name="email"
+                 dataErrorMsg="Email is Required" dataValidationRegex='^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$'
+                 dataRegexErrorMsg="Enter a valid Email addres" />
             </div>
         )
     }
--- index.js
+++ index.js
@@ -7,31 +7,51 @@ export class IntelliFields extends Component {
         super(props);
         this.state = {
             canShowErrorMsg: false,
-            errorMsg:""
+            errorMsg: ""
         };
         this.handleTouch = this.handleTouch.bind(this);
-        this.handleChange = this.handleChange.bind(this);       
-    } 
+        this.handleChange = this.handleChange.bind(this);
+    }
+    static defaultProps = {
+        callBackFunction: '',
+        selectedVal:'',
+        className:'intelli-fields',
+        autoComplete:'on',
+        minLength:'',
+        maxLength:'',
+        value:''
+    }
+    static propTypes = {
+        callBackFunction: PropTypes.string.isRequired,
+        selectedVal: PropTypes.string.isRequired,
+        className: PropTypes.string.isRequired,
+        autoComplete: PropTypes.string.isRequired,
+        minLength: PropTypes.string.isRequired,
+        maxLength: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired
+    }
+
+    // this.props.callBackFunction
     handleTouch(event) {
         var elmValue = event.target.value.trim();
         var validation = event.target.getAttribute("data-validation");
         var regex = RegExp(this.props.dataValidationRegex);
         if (!elmValue && validation == "true") {
             this.setState({
-              canShowErrorMsg: true,
-              errorMsg: this.props.dataErrorMsg?this.props.dataErrorMsg:'Required'
-            });            
-        }else if(regex && !regex.test(elmValue)){
+                canShowErrorMsg: true,
+                errorMsg: this.props.dataErrorMsg ? this.props.dataErrorMsg : 'Required'
+            });
+        } else if (regex && !regex.test(elmValue)) {
             this.setState({
                 canShowErrorMsg: true,
                 errorMsg: this.props.dataRegexErrorMsg
             });
-        }else {
-            this.setState({canShowErrorMsg: false});
+        } else {
+            this.setState({ canShowErrorMsg: false });
         }
-        
+
     }
-    handleChange(event){
+    handleChange(event) {
         const input = e.target;
         const name = input.name;
         const value = input.type === "checkbox" ? input.checked : input.value;
@@ -40,49 +60,49 @@ export class IntelliFields extends Component {
     render() {
         return (
             <>
-                {this.props.type == 'hidden'?(
-                   ""
-                ):(                    
-                    <>
-                    <label class="label" htmlFor={this.props.id}>{this.props.label}</label>
-                    {this.props.tooltip?<span title={this.props.tooltip} className="fas fa-question-circle"></span>:""}
-                   </>
-                )}
-                {this.props.type == 'dropdown'?(
+                {this.props.type == 'hidden' ? (
+                    <></>
+                ) : (
+                        <>
+                            <label class="label" htmlFor={this.props.id}>{this.props.label}</label>
+                            {this.props.tooltip ? <span title={this.props.tooltip} className="fas fa-question-circle"></span> : ""}
+                        </>
+                    )}
+                {this.props.type == 'dropdown' ? (
                     <div className="position-relative flex">
-                        <select defaultValue={this.props.selectedVal?this.props.selectedVal:""} onBlur={(e)=>this.handleTouch(e)} 
-                        type={this.props.type}
-                        data-validation={this.props.dataValidationRequired}
-                        id={this.props.id}
-                        className={this.props.className}
-                        onChange={this.props.callBackFunction?this.props.callBackFunction:''}
-                        name={this.props.name}
-                        disabled={this.props.disabled}>
-                            {this.props.options?(
-                                Object.entries(this.props.options).sort((a,b) => a[1] - b[1]).map(([key,value]) => {
+                        <select defaultValue={this.props.selectedVal} onBlur={(e) => this.handleTouch(e)}
+                            type={this.props.type}
+                            data-validation={this.props.dataValidationRequired}
+                            id={this.props.id}
+                            className={this.props.className}
+                            onChange={this.props.callBackFunction}
+                            name={this.props.name}
+                            disabled={this.props.disabled}>
+                            {this.props.options ? (
+                                Object.entries(this.props.options).sort((a, b) => a[1] - b[1]).map(([key, value]) => {
                                     return <option id={key} value={key}>{value}</option>
                                 })
-                                ):(
+                            ) : (
                                     <option value="" selected></option>
-                                    )}
-                        
+                                )}
+
                         </select>
                         <div className="select_box_chevron fas fa-chevron-down"></div>
                     </div>
-                ):(
-                    <input onChange={(e)=>this.handleTouch(e)} 
-                    type={this.props.type}
-                    data-validation={this.props.dataValidationRequired}
-                    id={this.props.id}
-                    className={this.props.className}
-                    disabled={this.props.disabled}
-                    name={this.props.name} 
-                    data-value={this.props.dataValue}
-                    defaultValue={this.props.value?this.props.value:""}
-                    minLength={this.props.minLength?this.props.minLength:""}
-                    maxLength={this.props.maxLength?this.props.maxLength:""}
-                    autoComplete={this.props.autoComplete?this.props.autoComplete:"on"} onBlur={this.props.callBackFunction?this.props.callBackFunction:''}/>
-                )}
+                ) : (
+                        <input onBlur={(e) => this.handleTouch(e)}
+                            type={this.props.type}
+                            data-validation={this.props.dataValidationRequired}
+                            id={this.props.id}
+                            className={this.props.className}
+                            disabled={this.props.disabled}
+                            name={this.props.name}
+                            data-value={this.props.dataValue}
+                            defaultValue={this.props.value}
+                            minLength={this.props.minLength}
+                            maxLength={this.props.maxLength}
+                            autoComplete={this.props.autoComplete} />
+                    )}
                 {this.state.canShowErrorMsg && (
                     <p className="field-error-message">{this.state.errorMsg}</p>
                 )}
